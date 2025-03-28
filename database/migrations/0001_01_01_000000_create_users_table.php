f4805937a6f8afa78eebb76e708a419472b3c627
Ahmad Mohammad Kouja <ahmadkoujaak7@hotmail.com>
2025-03-29 01:19:46 +0300

    PLS-1 update database structure

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->char('country_code', 3)->nullable();
-            $table->char('currency_code', 3)->nullable();
             $table->timestamps();
         });
 
